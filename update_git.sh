885632fbc00b03736ee42dbb389b63f0aceec1ca
dodspot <48319510+dodspot@users.noreply.github.com>
2020-01-30 21:14:53 +0100

    correction of pull-request

--- update_git.sh
+++ update_git.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-# grep up to date string
-check=$(git status /root/Debian_10_Scripts/ | grep -a "Your branch is up to date with")
-
-# check log and cut first line if neccessary
-tail -n 99 >update.log
-
-# if no string was found do
-if test -z "$check" 
-then
-
 # save working directory and change to git directory
 directory=$(pwd)
 cd /root/Debian_10_Scripts/
 
+# check for updates and cut log if 100
+git pull > nul
+tail -n 99 /root/Debian_10_Scripts/update.log > update.log
+
+# grep up to date string
+check=$(git status /root/Debian_10_Scripts/ | grep -a "Your branch is up to date with")
+
+# if no string was found do
+if test -z "$check"
+then
+
 # stash and pull quiet
 git stash -q
 git pull -q
-
-# go back to working directory
-cd $directory
 
 # modify file permissions
 chmod -R 110 /root/Debian_10_Scripts/
@@ -27,9 +25,11 @@ chmod -R 110 /root/Debian_10_Scripts/
 #report status
 echo "$(date) UPDATED" >> update.log
 echo "Branch has been updated."
-echo $check
 else
 echo "$(date) SKIPPED" >> update.log
 echo "No update available."
-echo $check
 fi
+
+# go back to working directory
+cd $directory
+echo $check
