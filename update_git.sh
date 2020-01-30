86d6e19de09608c73320e276c170e0fa3571c4ce
dodspot <48319510+dodspot@users.noreply.github.com>
2020-01-30 20:15:47 +0100

    Update update_git.sh

--- update_git.sh
+++ update_git.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-git stash /root/Debian_10_Scripts/
-git pull /root/Debian_10_Scripts/
-git status /root/Debian_10_Scripts/
 
+check=$(git status /root/Debian_10_Scripts/ | grep -a "Your branch is up to date with")
+if test -n "$check" then
+
+# save directory and change
+directory=$(pwd)
+cd /root/Debian_10_Scripts/
+
+# do git things
+git stash 
+git pull 
+
+# go back to directory
+cd $directory
+
+# modify file permissions
 chmod -R 110 /root/Debian_10_Scripts/
+
+#report status
+check=$(git status /root/Debian_10_Scripts/ | grep -a "Your branch is up to date with")
+fi
+
+echo $check
