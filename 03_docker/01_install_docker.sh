d4eab311386e2a7da66125a1e79589f13f6d3561
dodspot <48319510+dodspot@users.noreply.github.com>
2020-02-03 23:07:07 +0100

    optimize

--- 03_docker/01_install_docker.sh
+++ 03_docker/01_install_docker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Update and upgrade
+clear
 apt-get -y update
 apt-get -y upgrade
 
 # Install Apps
+clear
 apt-get -y install apt-transport-https
 apt-get -y install ca-certificates 
 apt-get -y install curl 
@@ -12,20 +14,17 @@ apt-get -y install gnupg2
 apt-get -y install software-properties-common
 	
 # Add librarys and repositorys
+clear
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - 
 curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 	
-# Make docker executable
+# Make docker executable update and install
 chmod +x /usr/local/bin/docker-compose
-
-# Update from docker
 apt-get -y update
-
-# Install docker
 apt-get -y install docker-ce
 
 # Finish
-dialog --ascii-lines --msgbox "Docker installation completed" 0 0
 clear
+dialog --ascii-lines --msgbox "Docker installation completed" 0 0
 bash /git/Debian_10_Scripts/03_docker/index.sh
