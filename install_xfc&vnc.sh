ff8a53bf6500411c0f133d1e29bdd8ec3d4f2683
dodspot <48319510+dodspot@users.noreply.github.com>
2020-02-03 15:57:54 +0100

    changed for xfc and include vnc

---
+++ install_xfc&vnc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# update and upgrade
+apt-get -y update
+apt-get -y upgrade
+
+# install xfc4 desktop environment
+apt-get -y install xfce4
+apt-get -y install xfce4-goodies
+
+# install vnc viewer
+apt-get -y install tightvncserver
+
+# initiate vnc server
+echo "Password must between six and eight characters"
+vncserver
+
+# stop vnc server
+vncserver -kill :1
+
+# backup vnc konfiguration
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+
+# create own konfig file
+echo "#!/bin/bash" > ~/.vnc/xstartup
+echo "xrdb $HOME/.Xresources" >>~/.vnc/xstartup
+echo "startxfce4 &" >>~/.vnc/xstartup
+
+# chmod own config file
+chmod +x ~/.vnc/xstartup
+
+# restart vncserver
+vncserver
