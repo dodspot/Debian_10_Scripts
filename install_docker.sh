8f40c725d7f1b90eb874d06b018eba27bc5fd82c
dodspot <48319510+dodspot@users.noreply.github.com>
2020-02-03 18:45:08 +0100

    added docker compose

--- install_docker.sh
+++ install_docker.sh
@@ -14,6 +14,10 @@ apt-get -qq install software-properties-common
 # add offizial Docker GPG
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
+# add Docker Compose Library
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # add Docker to aptitude sourcelist
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
