60be870045b3c55475462a18dd4416252e6203ec
dodspot <48319510+dodspot@users.noreply.github.com>
2020-02-03 18:45:28 +0100

    changed to docker-compose creation

--- install_docker_wordpress.sh
+++ install_docker_wordpress.sh
@@ -4,6 +4,34 @@
 apt-get -qq update
 apt-get -qq upgrade
 
-# install docker image
+# create wordpress directory
+mkdir /wordpress
+cd /wordpress
 
-docker pull wordpress
+# create docker-compose file
+echo"version: '3.1'" 				>  docker.compose.yml
+echo""						>> docker.compose.yml
+echo"services:"					>> docker.compose.yml
+echo"						>> docker.compose.yml
+echo"  wordpress:"				>> docker.compose.yml
+echo"    image: wordpress"			>> docker.compose.yml
+echo"    restart: always"			>> docker.compose.yml
+echo"    ports:"				>> docker.compose.yml
+echo"      - 8080:80"				>> docker.compose.yml
+echo"    environment:"				>> docker.compose.yml
+echo"      WORDPRESS_DB_PASSWORD: example"	>> docker.compose.yml
+echo""						>> docker.compose.yml
+echo"  mysql:"					>> docker.compose.yml
+echo"    image: mysql:5.7"			>> docker.compose.yml
+echo"    restart: always"			>> docker.compose.yml
+echo"    environment:"				>> docker.compose.yml
+echo"      MYSQL_ROOT_PASSWORD: example"	>> docker.compose.yml
+
+# start docker-compose
+docker-compose up -d
+
+# open Firewall port
+ufw allow 8080
+
+# goto root
+cd ~
