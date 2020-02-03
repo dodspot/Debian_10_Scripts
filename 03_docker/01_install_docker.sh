a5d9b77b5ed01eac8f8f24712752adbf590871c3
dodspot <48319510+dodspot@users.noreply.github.com>
2020-02-03 23:04:38 +0100

    make setup loud

--- 03_docker/01_install_docker.sh
+++ 03_docker/01_install_docker.sh
@@ -1,48 +1,29 @@
 #!/bin/bash
 
 # Update and upgrade
-dialog --ascii-lines --gauge "Update" 0 0 8
-apt-get -qq update
-
-dialog --ascii-lines --gauge "Upgrade" 0 0 16
-apt-get -qq upgrade
+apt-get -y update
+apt-get -y upgrade
 
 # Install Apps
-dialog --ascii-lines --gauge "Install apt-transport-https" 0 0 24 
-apt-get -qq install apt-transport-https
-
-dialog --ascii-lines --gauge "Install ca-certificates" 0 0 32 
-apt-get -qq install ca-certificates 
-
-dialog --ascii-lines --gauge "Install curl" 0 0 40 
-apt-get -qq install curl 
-
-dialog --ascii-lines --gauge "Install gnupg2" 0 0 48 
-apt-get -qq install gnupg2 
-
-dialog --ascii-lines --gauge "Install software-properties-common" 0 0 56 
-apt-get -qq install software-properties-common
+apt-get -y install apt-transport-https
+apt-get -y install ca-certificates 
+apt-get -y install curl 
+apt-get -y install gnupg2 
+apt-get -y install software-properties-common
 	
-# Add Librarys
-dialog --ascii-lines --gauge "Add librarys" 0 0 64
-curl --silent -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - 
-curl --silent -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
-
-# Add repository
-dialog --ascii-lines --gauge "" 0 0 72
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /dev/null 2>&1
+# Add librarys and repositorys
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - 
+curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 	
 # Make docker executable
-dialog --ascii-lines --gauge "Make docker executable" 0 0 80
 chmod +x /usr/local/bin/docker-compose
 
 # Update from docker
-dialog --ascii-lines --gauge "Update from docker" 0 0 88
-apt-get -qq update
+apt-get -y update
 
 # Install docker
-dialog --ascii-lines --gauge "Install docker" 0 0 96
-apt-get -qq install docker-ce
+apt-get -y install docker-ce
 
 # Finish
 dialog --ascii-lines --msgbox "Docker installation completed" 0 0
